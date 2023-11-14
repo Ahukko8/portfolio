95b1271bc1fb6a70e090a14578e875fd5469b87c
Ahukko8 <ahukko88@gmail.com>
2023-11-14 09:49:25 +0500

    hero page editing

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,16 +1,19 @@
+import { CustomLink } from "./CustomLink";
+
 export function HeroSection() {
   return (
     <>
-      <section className="grid items-center h-[calc(100vh-70px)] max-h-[678px]">
+      <section className="grid md:grid-cols-2 items-center h-[calc(100vh-70px)] max-h-[678px]">
         <div>
-          <p>Hello, I am</p>
-          <h1>Ibrahim Ahusan Mohamed</h1>
+          <p className="text-lg mb-1">Hello, I am</p>
+          <h1 className="font-title tracking-wider text-3xl sm:text-6xl -ml-0.5">Ibrahim Ahusan Mohamed</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            quaerat quisquam dolorem aut quis saepe nam repellat cupiditate
-            repellendus animi quidem, asperiores quae sequi. Repellendus eaque
-            optio blanditiis vitae molestiae.
+            As a self-taught aspiring developer, I am passionate about crafting
+            innovative solutions through code. Eager to broaden my experience, I
+            actively seek opportunities for both work and training to enhance my
+            skills and contribute to the ever-evolving tech landscape.
           </p>
+          <CustomLink href={"About Me"}>About Me</CustomLink>
         </div>
       </section>
     </>
